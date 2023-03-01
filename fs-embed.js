f1065ab6b4f782c73223fb847be45c79965cf78b
Flex01a <91494774+Flex01a@users.noreply.github.com>
2023-03-01 21:35:29 +0300

    Update fs-embed.js

--- fs-embed.js
+++ fs-embed.js
@@ -1,4 +1,4 @@
-var fs_embed = {
+var fsembed = {
   generate: function(url, width, height) {
     var embedCode = '<div style="position:relative;padding-bottom:' + (height / width * 100) + '%;height:0;">' +
                       '<iframe src="' + url + '" frameborder="0" style="position:absolute;width:100%;height:100%;top:0;left:0;"></iframe>' +
